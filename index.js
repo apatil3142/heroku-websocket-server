11c41c3bdc4e053c810b0d4e720160448260595a
apatil3142 <patilaniket2236@gmail.com>
2024-10-14 01:35:29 +0530

    websocket server

---
+++ index.js
@@ -0,0 +1,51 @@
+
+import { Server } from 'socket.io';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const io = new Server(8000, {
+  cors: {
+    origin: process.env.BOOKSWAP_DOMAIN,
+  },
+});
+
+let users = [];
+
+const addUser = (userId, socketId) => {
+  if(!users.some((user) => user.userId === userId)) users.push({ userId, socketId });
+};
+
+const removeUser = (socketId) => {
+  users = users.filter((user) => user.socketId !== socketId);
+};
+
+const getUser = (userId) => {
+  return users.find((user) => user.userId === userId);
+};
+
+io.on('connection', (socket) => {
+  // Handle socket events here
+  //take userId and socketId from user
+  console.log('user connected');
+  socket.on("addUser", (userId) => {
+    addUser(userId, socket.id);
+    io.emit("getUsers", users);
+  });
+
+  //send and get message
+  socket.on("sendMessage", ({ senderId, receiverId, text }) => {
+    const user = getUser(receiverId);
+    if(user){
+      io.to(user.socketId).emit("getMessage", {
+        senderId,
+        text,
+      });
+    }
+  });
+
+  //when disconnect
+  socket.on("disconnect", () => {
+    removeUser(socket.id);
+    io.emit("getUsers", users);
+  });
+});
